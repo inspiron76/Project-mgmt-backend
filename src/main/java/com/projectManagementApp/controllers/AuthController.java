350e66721627841b1c7ec3e6224732d6fc671996
Aachaman <144558579+Achamanp@users.noreply.github.com>
2025-11-13 20:54:10 +0530

    change db configuration

--- src/main/java/com/projectManagementApp/controllers/AuthController.java
+++ src/main/java/com/projectManagementApp/controllers/AuthController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
